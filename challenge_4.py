73559409ac9e007afff34ac3688f4303f0fd3047
SarinSwift <sarinyaswift@gmail.com>
2019-08-06 18:19:15 -0700

    completed minimum path sum dynamic programming problem

--- challenge_4.py
+++ challenge_4.py
@@ -41,7 +41,7 @@ def knapsack(C, items):
             knap_items.append(item.name)
             return (val_with_item, knap_items)
         else:
-            # return the tuple with the same value (because we didn't add an additional item to our list) and the list of items 
+            # return the tuple with the same value (because we didn't add an additional item to our list) and the list of items
             return (val_without_item, knap__without_items)
 
 items = [Item("boot", 10, 60),
@@ -49,3 +49,29 @@ items = [Item("boot", 10, 60),
         Item("water", 30, 120),
         Item("first aid", 15, 70)]
 print(knapsack(50, items))
+
+
+# recursive function through dynamic programming finding the minimum value path
+def min_path_sum(grid, i, j):
+    '''
+    starts at 0,0
+    ex. [[1,3,1],
+        [1,5,1],
+        [4,2,1]]
+    should return a value of 7 from the shortest path: 1->3->1->1->1
+    '''
+    # means that we've hit the end!
+    if i == len(grid) - 1 and j == len(grid) - 1:
+        return grid[i][j]
+    # means that we've hit the last row, and now we can only go to the right
+    elif i == len(grid) - 1:
+        return min_path_sum(grid, i, j + 1) + grid[i][j]
+    # means that we've hit the last possible column, and now we can only go down
+    elif j == len(grid) - 1:
+        return min_path_sum(grid, i + 1, j) + grid[i][j]
+    else:
+        down = min_path_sum(grid,i + 1, j)
+        right = min_path_sum(grid, i, j + 1)
+        return min(down, right) + grid[i][j]
+
+print(min_path_sum([[1,3,1], [1,5,1], [4,2,1]], 0, 0))
