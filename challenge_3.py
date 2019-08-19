cdc7728bd7dabc258e2e8816c2042bdcd6a8b5ed
SarinSwift <sarinyaswift@gmail.com>
2019-08-19 11:19:48 -0700

    added challenge3 with dfs to find path

--- challenge_3.py
+++ challenge_3.py
@@ -1,27 +1,53 @@
-
+import sys
+import queue
+from ast import literal_eval as make_tuple
 
 class Vertex:
 
-    def __init__(self, v):
-        self.name = v
-        self.neighbors = set()
+    def __init__(self, v_name):
+        self.name = v_name
+        self.neighbors = {}     # key: neigbor vertex object, value: weight of connecting edge
         self.parent = None
 
-    def add_neighbor(self, v):
-        # print("adding neighbor:" + str(v.name))
-        self.neighbors.add(v)
+    def add_neighbor(self, v, w=0):
+        '''Given input is already a vertex object.
+            v: the connecting neighbor, w: the weight
+        '''
+        if v not in self.neighbors:
+            self.neighbors[v] = w
+
+    def __str__(self):
+        """Output the list of neighbors of this vertex."""
+        return f"{self.name} adjacent to {[x.name for x in self.neighbors]}"
+
+    def get_neighbors(self):
+        """Return the neighbors of this vertex."""
+        return self.neighbors.keys()
+
+    def get_edge_weight(self, vertex):
+        """Return the weight of this connecting edge between self and the given vertex object."""
+        return self.neighbors[vertex]
 
 class Graph:
     vertices = {}
 
-    def add_vertex(self, vertex):
-        '''Given input should already be a vertex object'''
-        self.vertices[vertex.name] = vertex         # {vertex name: vertex}
+    def add_vertex(self, name):
+        '''Given input is the name of the vertex'''
+        self.vertices[name] = Vertex(name)
+        return self.vertices[name]
 
-    def add_edge(self, from_vert, to_vert):
-        if from_vert.name in self.vertices and to_vert.name in self.vertices:
-            from_vert.add_neighbor(to_vert)
-            to_vert.add_neighbor(from_vert)
+    def add_edge(self, name1, name2, cost=0):
+        # insert the new vertex of object1 if not yet in the graph
+        if name1 not in self.vertices:
+            self.add_vertex(name1)
+        # insert the new vertex of object2 if not yet in the graph
+        if name2 not in self.vertices:
+            self.add_vertex(name2)
+
+        # now we're sure there are both vertices in the graph, we can add negihbors to both
+        object1 = self.vertices[name1]
+        object2 = self.vertices[name2]
+        object1.add_neighbor(object2, cost)
 
 
     def is_connected_iterative(self, vertex):
@@ -120,11 +146,11 @@ class Graph:
 
         if from_vert not in visited:
             visited.add(from_vert)
-            print(from_vert)
             for neighbor in from_vert.neighbors:
-                dfs_recursive(neighbor, to_vert, visited)
+                self.dfs_recursive(neighbor, to_vert, visited)
 
-        return visited
+        print(list(visited))
+        return list(visited) is not None
 
     def __iter__(self):
         """
@@ -133,41 +159,64 @@ class Graph:
         """
         return iter(self.vertices.values())
 
-def main():
-
-    text_file = sys.argv[1]
-    from_vert = sys.argv[2]
-    to_vert = sys.argv[3]
-
-    graph = Graph()
-
-    with open('graph_data.txt', 'r') as file:
+def read_from_file(filename):
+    with open(filename, 'r') as file:
         lines = file.readlines()
 
-        for x in lines:
-            if x[0].isalpha():
-                continue
-            if x[0].isdigit():          # making sure this is the line with all the vertices
-                # create Graph by looping through the line that contains all the vertices
-                for key in x.strip().split(","):     # key is 1, 2, 3, 4
-                    graph.add_vertex(Vertex(key))
-            else:
-                # is the neighbors of the vertex at str[0]
-                tuple__neighbors = make_tuple(x)        # tuple__neighbors is (1,2,10)
+        # make sure it's a graph
+        graph_str =  lines[0].strip() if len(lines) > 0 else None
+        if graph_str != "G":
+            raise Exception("File must start with G.")
+        # is_bidirectional = graph_or_digraph_str == "G"
 
-                # Make sure we're grabbing the correct vertex by getting it from the populated graph's
-                v_from = graph.vertices[str(tuple__neighbors[0])]
-                v_to = graph.vertices[str(tuple__neighbors[1])]
+        graph = Graph()
 
-                # v_from = Vertex(str(tuple__neighbors[0]))
-                # v_to = Vertex(str(tuple__neighbors[1]))
-                # print(v_from)
-                graph.add_edge(v_from, v_to)
+        # add the vertices from the 2nd line
+        for vertex_name in lines[1].strip("() \n").split(","):
+            graph.add_vertex(vertex_name)
 
-        # print("main, graph.vertices: ", graph.vertices)
-        # print("main, graph.vertices object 1: ", list(graph.vertices['1'].neighbors))
+        # add the edges by looping through the remaing lines in the file
+        # the line is (1,2,10) and so on...
+        for line in lines[2:]:
+            # getting rid of ',' and combining all the 2/3 values together
+            new_edge = line.strip("() \n").split(",")
+            if len(new_edge) < 2 or len(new_edge) > 3:
+                raise Exception("Lines adding edges must include 2 or 3 values")
 
-    return print(graph.bfs(graph.vertices[from_vert], graph.vertices[to_vert]))
+            # Get vertices from 'new_edge' from the first index to the second.
+            vertex1, vertex2 = new_edge[:2]
+
+            # Get weight if the len of 'new_edge' is 3. If the length is not 3, weight value will be None
+            weight = int(new_edge[2]) if len(new_edge) == 3 else None
+
+            # Add edge(s)!!
+            graph.add_edge(vertex1, vertex2, weight)
+            graph.add_edge(vertex2, vertex1, weight)
+
+        return graph
+
+def main():
+
+    # read the file from argument given
+    filename = sys.argv[1]
+    g = read_from_file(filename)
+
+    # grabing the name of the vertex
+    a_name = sys.argv[2]
+    b_name = sys.argv[3]
+    # grabing the vertex object
+    if a_name in g.vertices and b_name in g.vertices:
+        a = g.vertices[a_name]
+        b = g.vertices[b_name]
+    else:
+        a = None
+        b = None
+        print("Error: No vertex with that given item in the graph")
+        return False
+
+    # returns True if there's a path between these vertices
+    print(g.dfs_recursive(a, b))
+
 
 if __name__ == '__main__':
     main()
